1e971286fccea078bd97b9c7115be4bbab58721d
vkopachinsy <vlad.kopachinsky@gmail.com>
2015-09-18 19:56:43 +0300

    fixed style

--- Validator.UI/Validator.UI/Operations/TestCommand.cs
+++ Validator.UI/Validator.UI/Operations/TestCommand.cs
@@ -4,26 +4,26 @@
     using Incoding;
     using Incoding.CQRS;
 
-    public class TestCommand : CommandBase
-    {
-        public string NotEmpty { get; set; }
-
-        public string Must { get; set; }
-
-        public string ThrowFromCommand { get; set; }
-
-        public override void Execute()
-        {
-            throw IncWebException.For<TestCommand>(command => command.ThrowFromCommand, "Throw form command");
-        }
-
-        public class Validator : AbstractValidator<TestCommand>
-        {
-            public Validator()
-            {
-                RuleFor(r => r.NotEmpty).NotEmpty();
-                RuleFor(r => r.Must).Must(s => s != "Test").WithMessage("Value it is 'Test'");
-            }
-        }
-    }
+public class TestCommand : CommandBase
+{
+  public string NotEmpty { get; set; }
+  
+  public string Must { get; set; }
+  
+  public string ThrowFromCommand { get; set; }
+  
+  public override void Execute()
+  {
+      throw IncWebException.For<TestCommand>(command => command.ThrowFromCommand, "Throw form command");
+  }
+  
+  public class Validator : AbstractValidator<TestCommand>
+  {
+      public Validator()
+      {
+          RuleFor(r => r.NotEmpty).NotEmpty();
+          RuleFor(r => r.Must).Must(s => s != "Test").WithMessage("Value it is 'Test'");
+      }
+  }
+}
 }
